ccd049009cf22b288115127ae739c9257c7d75d8
Chris Rabe <chris.rabe14@gmail.com>
2021-06-24 20:48:41 +1000

    added event resolver

--- src/modules/event/resolvers/event.resolver.js
+++ src/modules/event/resolvers/event.resolver.js
@@ -1,16 +1,52 @@
-const Query = {
-  myType: (parent, args, context) => ({
-    id: "my-type",
-    name: "My Type",
-  }),
+export const EventType = {
+  giveaway: "GIVEAWAY",
+  foodDrive: "FOODDRIVE",
+  request: "REQUEST",
 };
 
-const MyType = {
+// General event resolver
+
+const creator = (parent) =>
+  // TODO query DB for user
+  parent.creator;
+
+const eventResolvers = {
   id: (parent) => parent.id,
-  name: (parent) => parent.name,
+  type: (parent) => parent.type,
+  creator,
+  datetime: (parent) => parent.datetime,
+  location: (parent) => parent.location,
+};
+
+// Food event resolvers
+
+const food = (parent) =>
+  // TODO search food items in DB using IDs
+  parent.food;
+
+const attendees = (parent) =>
+  // TODO search for attendees in DB using IDs
+  parent.attendees;
+
+const totalServings = (parent) =>
+  // TODO add up all servings left in food list
+  0;
+
+const FoodEvent = {
+  ...eventResolvers,
+  food,
+  attendees,
+  totalServings,
+};
+
+// Request event resolvers
+
+const RequestEvent = {
+  ...eventResolvers,
+  allergies: (parent) => parent.allergies,
 };
 
 export default {
-  Query,
-  MyType,
+  FoodEvent,
+  RequestEvent,
 };
