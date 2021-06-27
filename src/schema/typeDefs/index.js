d8875ad2850051a4df3e2fc1be4965a52255d790
Chris Rabe <chris.rabe14@gmail.com>
2021-06-27 19:24:19 +1000

    refactored

--- src/schema/typeDefs/index.js
+++ src/schema/typeDefs/index.js
@@ -1,13 +1,7 @@
 import path from "path";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeTypeDefs } from "@graphql-tools/merge";
-import locationType from "../../shared/typeDefs/location.graphql";
-import eventType from "../../shared/typeDefs/event.graphql";
 
 const types = loadFilesSync(path.join(__dirname, "./**/*.graphql"));
-
-// add additional shared depedencies
-types.push(eventType);
-types.push(locationType);
 
 export default mergeTypeDefs(types);
