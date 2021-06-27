063eb75489dc406ac1665bdd479a6b789d9929d6
Winnie <winnie.zheng@accenture.com>
2021-06-27 20:52:28 +1000

    fix: cleanup exports

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 import Cloudant from "@cloudant/cloudant";
 import { cloudant as config } from "../config";
 import createLogger from "../utils/logger";
+import CloudantDatabase from "./cloudant";
 
 const logger = createLogger({
   name: "cloudant-logger",
@@ -15,36 +16,35 @@ const cloudantConfig = {
   },
 };
 
-export const connectCloudantDatabase =  () => {
-    return Cloudant(cloudantConfig, (err, cloudant) => {
-      if (err) {
-        logger.error(
-          `Connect failure: ${err.message} for Cloudant ID: ${config.cloudantId}`
-        );
-        return error;
-      } else {
-        (async () => {
-          try {
-            const existingDbs = await cloudant.db.list();
-            if (!existingDbs.includes(config.dbName)) {
-              logger.info(
-                `Database does not exist... creating: ${config.dbName}`
-              );
-              await cloudant.db.create(config.dbName);
-            }
-            const db = cloudant.use(config.dbName);
+const connectCloudantDatabase = () => {
+  return Cloudant(cloudantConfig, (err, cloudant) => {
+    if (err) {
+      logger.error(
+        `Connect failure: ${err.message} for Cloudant ID: ${config.cloudantId}`
+      );
+      return error;
+    } else {
+      (async () => {
+        try {
+          const existingDbs = await cloudant.db.list();
+          if (!existingDbs.includes(config.dbName)) {
             logger.info(
-              `Connection success! Connected to DB: ${config.dbName}`
+              `Database does not exist... creating: ${config.dbName}`
             );
-            return db;
-          } catch (error) {
-            logger.error(
-              `Connect failure: ${err.message} for Cloudant ID: ${config.cloudantId}`
-            );
-            return error;
+            await cloudant.db.create(config.dbName);
           }
-        })();
-      }
-    });
-}
+          const db = cloudant.use(config.dbName);
+          logger.info(`Connection success! Connected to DB: ${config.dbName}`);
+          return db;
+        } catch (error) {
+          logger.error(
+            `Connect failure: ${err.message} for Cloudant ID: ${config.cloudantId}`
+          );
+          return error;
+        }
+      })();
+    }
+  });
+};
 
+export default new CloudantDatabase(connectCloudantDatabase());
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import { ApolloServer, PubSub } from "apollo-server";
 import modules from "./modules";
 import plugins from "./plugins";
-import { connectCloudantDatabase } from "./database";
-import  CloudantDatabase from "./database/cloudant";
-
+import database from "./database";
 
 //connectCloudantDatabase connects to the cloudant db and returns thhe database
-const database = new CloudantDatabase(connectCloudantDatabase());
 // Anything shared between resolvers are put into context
 const server = new ApolloServer({
   plugins,
