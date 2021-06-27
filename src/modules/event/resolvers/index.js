423fd80fc0dd73c215ff84a8e5932effcb514e88
Chris Rabe <chris.rabe14@gmail.com>
2021-06-27 18:58:31 +1000

    pulled out event to shared

--- src/modules/event/resolvers/index.js
+++ src/modules/event/resolvers/index.js
@@ -2,10 +2,12 @@ import path from "path";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers } from "@graphql-tools/merge";
 import locationResolver from "../../shared/resolvers/location.resolver";
+import eventResolver from "../../shared/resolvers/event.resolver";
 
 const resolvers = loadFilesSync(path.join(__dirname, "./**/*.resolver.*"));
 
 // Add shared resolvers
+resolvers.push(eventResolver);
 resolvers.push(locationResolver);
 
 export default mergeResolvers(resolvers);
