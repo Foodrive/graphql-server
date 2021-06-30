15d6ee829859535959f80af931668ba92e89a467
Andrew <a.a.chan@accenture.com>
2021-06-30 18:17:41 +1000

    fix: graphql errors

--- src/schema/resolvers/shared/location.resolver.js
+++
@@ -1,9 +0,0 @@
-const Location = {
-  address: (parent) => parent.address,
-  longitude: (parent) => parent.longitude,
-  latitude: (parent) => parent.latitude,
-};
-
-export default {
-  Location,
-};
