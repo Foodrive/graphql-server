045bd66575a7fed7e4f47d9e46338d64d1245bc6
Winnie <winnie.zheng@accenture.com>
2021-07-04 21:24:34 +1000

    cleanup: syntax stuff

--- src/schema/resolvers/invitation/subscription.resolver.js
+++ src/schema/resolvers/invitation/subscription.resolver.js
@@ -1,22 +1,22 @@
 import { invitationTriggers } from "utils/pubSubTriggers";
 
 const createdInvitation = {
-  subscribe: (_0, args, { pubsub }) =>
-    pubsub.asyncIterator(invitationTriggers.createdInvitation),
+  subscribe: (_, __, context) =>
+    context.pubsub.asyncIterator(invitationTriggers.createdInvitation),
 };
 
 const acceptedInvitation = {
-  subscribe: (_0, _1, { pubsub }) =>
-    pubsub.asyncIterator(invitationTriggers.acceptedInvitation),
+  subscribe: (_, __, context) =>
+    context.pubsub.asyncIterator(invitationTriggers.acceptedInvitation),
 };
 const rejectedInvitation = {
-  subscribe: (_0, _1, { pubsub }) =>
-    pubsub.asyncIterator(invitationTriggers.rejectedInvitation),
+  subscribe: (_, __, context) =>
+    context.pubsub.asyncIterator(invitationTriggers.rejectedInvitation),
 };
 
 const cancelledInvitation = {
-  subscribe: (_0, _1, { pubsub }) =>
-    pubsub.asyncIterator(invitationTriggers.cancelledInvitation),
+  subscribe: (_, __, context) =>
+    context.pubsub.asyncIterator(invitationTriggers.cancelledInvitation),
 };
 const Subscription = {
   createdInvitation,
