68d9536ba182c3a139df64b550f2786bccab2b4c
tomas.lawton <tomas.lawton@accenture.com>
2021-07-05 17:21:02 +1000

    add resolvers

--- src/schema/resolvers/event/request/request.resolver.js
+++ src/schema/resolvers/event/request/request.resolver.js
@@ -3,8 +3,14 @@ const location = async (parent, _, context) => {
   return data;
 };
 
+const requestor = async (parent, _, context) => {
+  const { data } = await context.database.requestor.getById(parent.requestor);
+  return data;
+};
+
 const Request = {
   location,
+  requestor,
 };
 
 export default {
