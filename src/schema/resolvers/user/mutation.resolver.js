efe66b2dd968d68561f7769596e610e0a556ca88
Andrew Truong <andrew.b.truong@accenture.com>
2021-07-08 06:52:22 +1000

    added checks for password and userid

--- src/schema/resolvers/user/mutation.resolver.js
+++ src/schema/resolvers/user/mutation.resolver.js
@@ -2,20 +2,28 @@ import { ApolloError } from "apollo-server-errors";
 import { hash } from "utils/auth";
 
 const updateUser = async (__, args, context) => {
-  const password = await hash(args.password);
 
-  const { data: result } = await context.database.users.update(context.userId, {
-    username: args.username,
-    password,
-    phoneNumber: args.phoneNumber,
-    email: args.email,
-    allergies: args.allergies,
-  });
+  let password = "";
+  if (password !== undefined || password !== null) {
+    password = await hash(args.password);
+  }
+  if (context.userId){
+    const { data: result } = await context.database.users.update(
+      context.userId,
+      {
+        username: args.username,
+        password,
+        phoneNumber: args.phoneNumber,
+        email: args.email,
+        allergies: args.allergies,
+      }
+    );
 
-  if (!result) {
-    throw new ApolloError("User update failed");
-  } else {
-    return result;
+    if (!result) {
+      throw new ApolloError("User update failed");
+    } else {
+      return result;
+    }
   }
 };
 
