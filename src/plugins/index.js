410b1cb5ff14870ddba2697e03690dae5c4542d0
Chris Rabe <chris.rabe14@gmail.com>
2021-07-17 15:22:05 +1000

    added landing page plugins

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,7 +1,17 @@
 // Import plugins here
 // To make own plugin, please go to
 // https://www.apollographql.com/docs/apollo-server/integrations/plugins/#server-lifecycle-events
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from "apollo-server-core";
 import eventLogger from "./event.logger";
 import serverLogger from "./server.logger";
 
-export default [eventLogger, serverLogger];
+export default [
+  eventLogger,
+  serverLogger,
+  process.env.NODE_ENV === "production"
+    ? ApolloServerPluginLandingPageProductionDefault({ footer: false })
+    : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+];
