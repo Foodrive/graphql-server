cbc05ae6b7fd9dd3a328c2eeb2a38780c2d35ebc
Chris Rabe <chris.rabe14@gmail.com>
2021-09-09 20:11:27 +1000

    feat: food drives returned from earliest to latest

--- src/schema/resolvers/event/foodDrive/query.resolver.js
+++ src/schema/resolvers/event/foodDrive/query.resolver.js
@@ -2,20 +2,21 @@ import { EventType } from "../../../../utils/constants";
 
 const Query = {
   getFoodDrives: async (_, args, context) => {
-    const { data } = await context.database.events.find(
-      args.userId
-        ? {
-            organiserId: args.userId,
-            type: EventType.foodDrive,
-          }
-        : {
-            type: EventType.foodDrive,
-          }
-    );
+    const query = { type: EventType.foodDrive };
+    if (args.userId) {
+      query.organiserId = args.userId;
+    }
+
+    const { data } = await context.database.events.find(query);
+    // Sort from earliest to latest
+    data.sort((a, b) => b.startDate.localeCompare(a.startDate));
     return data;
   },
+
   getFoodDriveById: async (_, args, context) => {
     const { data } = await context.database.events.getById(args.eventId);
+    // Sort from earliest to latest
+    data.sort((a, b) => b.startDate.localeCompare(a.startDate));
     return data;
   },
 };
